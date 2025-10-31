bf75e50d8e6e90a244aefd4953d7b4823d7ec816
Jared Woodcock <jared.woodcock@yahoo.com>
2025-10-31 16:23:48 -0500

    Move public files to root for GitHub Pages

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 async function openDb() {
   return open({
